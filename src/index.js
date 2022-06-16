323efbc98f726781ec79cc31b1ddce9808679437
lynda-nneka <anaedobelyn001@gmail.com>
2022-06-16 05:08:22 -0700

    javascript for prime numbers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // complete the function
 function prime (num) {
   // code goes here
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    } return true;
+  }
 }
 
 function solution (arg) {
